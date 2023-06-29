a272f0fca9249e5fd58ea23078b91eb49baa5067
Jack Aakash <jackaakash4@gmail.com>
2023-06-29 21:07:53 +0545

    STL list

---
+++ STLlist.cpp
@@ -0,0 +1,13 @@
+//list container
+#include <iostream>
+#include <list>
+using namespace std;
+
+int main(){
+    list <int> gqlist{12, 45, 8, 6};
+
+    for (auto i : gqlist){
+        cout << i << " ";
+    }
+    return 0;
+}
